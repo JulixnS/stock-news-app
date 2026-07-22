8811efe71f67ac2a8de440eca269f265b8d7f8ee
JulixnS <julianvss12@gmail.com>
2026-07-22 14:28:25 -0400

    tickers/ticker returns latest sentiment of ticker. tickers/ticker/articles returns the articles used to make the sentiment

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
+from sources.data import *
 app = FastAPI()
 
 @app.get("/")
 def root():
     return "Hello World"
+
+@app.get("/tickers/{ticker}")
+def get_data(ticker: str):
+    return read(ticker)
+
+@app.get("/tickers/{ticker}/articles")
+def articles(ticker, label: str | None = None):
+    return get_articles(ticker, label)
